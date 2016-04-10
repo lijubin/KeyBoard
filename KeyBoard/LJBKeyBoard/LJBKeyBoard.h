26deb092cd2eb289dafc4808e8b8744d82237e62
lijubin <lijubin@users.noreply.github.com>
2016-04-10 14:02:40 +0800

    Update LJBKeyBoard.h

--- KeyBoard/LJBKeyBoard/LJBKeyBoard.h
+++ KeyBoard/LJBKeyBoard/LJBKeyBoard.h
@@ -11,17 +11,6 @@
 @protocol LJBKeyBoardDelegate <NSObject>
 
 /**
- *  获取根控制器 self.view
- */
-- (UIView *)ljbKeyBoardSuperView;
-
-/**
- * scrollView的偏移量 scrollView 或者 tableView
- * 没有scrollView时，请传nil
- */
-- (UIScrollView *)ljbKeyBoardScrollView;
-
-/**
  *  textField的最大Y值 CGRectGetMaxY(textField.frame)
  */
 - (CGFloat)ljbKeyBoardGetMaxY;
@@ -30,7 +19,17 @@
 
 @interface LJBKeyBoard : UIView
 
-
+/**
+*  定义在viewDidLoad 视图加载完成中
+*
+*  @param superView  self.view 键盘弹出层
+*  @param scrollView scrollView的偏移量 scrollView 或者 tableView
+                     没有scrollView时，请传nil
+*  @param delegate   self 控制器
+*
+*  @return 返回值不需要接收
+*/
++ (instancetype)keyBoardWithSuperView:(UIView *)superView scrollView:(UIScrollView *)scrollView delegate:(id)delegate;
 
 @property(assign)id<LJBKeyBoardDelegate>delegate;
 
